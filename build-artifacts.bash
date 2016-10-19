4e2a4741fca643c7ee30a61cab427bdcb6d6881b
Jordan Hornblow <jordan@jch254.com>
2016-10-19 21:46:37 +1100

    Update API URI

--- build-artifacts.bash
+++ build-artifacts.bash
@@ -1,4 +1,4 @@
 npm install
 export NODE_ENV=production
-export API_BASE_URI="https://5p7z1m4of5.execute-api.ap-southeast-2.amazonaws.com/prod"
+export API_BASE_URI="https://34yerz3mq3.execute-api.ap-southeast-2.amazonaws.com/prod"
 npm run build
