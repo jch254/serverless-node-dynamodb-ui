260002d84219c8fd0c4a2c8fb8567c0b7a668e84
Jordan Hornblow <jordan@jch254.com>
2017-01-20 18:18:25 +1100

    Always go to items page after login

--- src/auth/sagas.js
+++ src/auth/sagas.js
@@ -1,6 +1,6 @@
 import Auth0Lock from 'auth0-lock';
 import 'isomorphic-fetch';
-import { call, put, take, select } from 'redux-saga/effects';
+import { call, put, take } from 'redux-saga/effects';
 import { push } from 'react-router-redux';
 import Immutable from 'immutable';
 
@@ -12,7 +12,7 @@ import {
   loginFailure,
   loginSuccess,
 } from './reducer';
-import { setStoredAuthState, removeStoredAuthState, getCurrentPath } from '../utils';
+import { setStoredAuthState, removeStoredAuthState } from '../utils';
 
 export function* loginRequestSaga() {
   const lock = new Auth0Lock(process.env.AUTH0_CLIENT_ID, process.env.AUTH0_DOMAIN, { auth: { redirect: false } });
@@ -43,11 +43,9 @@ export function* loginRequestSaga() {
   try {
     const { profile, idToken } = yield call(showLock);
 
-    const path = yield select(getCurrentPath);
-
     yield put(loginSuccess(profile, idToken));
     yield put(push('/'));
-    yield put(push((path === '/' ? '/items' : path)));
+    yield put(push('/items'));
   } catch (error) {
     yield put(loginFailure(error));
     yield put(push('/'));
