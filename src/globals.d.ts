914c2031f353b98510fcae49626fb720eadd6cef
Jordan Hornblow <jordan@jch254.com>
2017-08-29 20:04:55 +1000

    Remove d.ts ignore

---
+++ src/globals.d.ts
@@ -0,0 +1,6 @@
+declare module 'reflexbox';
+declare module 'react-loading';
+declare module 'inline-chunk-manifest-html-webpack-plugin';
+declare module 'webpack-chunk-hash';
+declare module 'react-geomicons';
+declare module 'react-expanding-textarea';
